ef2a232729650a4a1ee01e1586d00aff680d0260
Muhammad Ali <muhammadalimehboob54@gmail.com>
2026-07-15 10:58:19 -0700

    Update Todo app UI and functionality

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-todo-app-me.netlify.app/",
     credentials: true,
   }),
 );
